1c7ac1a95d6a931ba5e809866cd6c3821bdca453
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2010-07-26 13:05:46 +0200

    Compartir

--- app/models/shared_list.rb
+++ app/models/shared_list.rb
@@ -2,9 +2,9 @@ class SharedList < ActiveRecord::Base
   belongs_to :space
   belongs_to :subscriber_list
 
-  validates_presence_of :space_id, :expires_at
+  validates_presence_of :space_id#, :expires_at
 
-  def validate
-    errors.add(:expires_at, "La fecha debe ser mayor o igual al día de hoy") if self.expires_at < Date.today
-  end
+  # def validate
+  #   errors.add(:expires_at, "La fecha debe ser mayor o igual al día de hoy") if self.expires_at < Date.today
+  # end
 end
