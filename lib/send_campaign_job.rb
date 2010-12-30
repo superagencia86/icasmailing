04dae1a1632d1a874a0a6e8a2071248be7b17baa
danigb <danigb@gmail.com>
2010-12-30 14:28:51 +0100

    sent mail doesn't prevent to send again

--- lib/send_campaign_job.rb
+++ lib/send_campaign_job.rb
@@ -1,13 +1,20 @@
+
 class SendCampaignJob < Struct.new(:campaign_id)
   include ActionView::Helpers::UrlHelper
   
   # Actualizamos una categoría
   def perform
+    logger = ActiveRecord::Base.logger
+    logger.debug "PERFORM SendCampaignJob"
+    logger.debug "CAMPAIGN ID #{campaign_id}"
     campaign = Campaign.find(campaign_id)
     emails = []
+    logger.debug "CAMPAIGN #{campaign.to_yaml}"
     campaign.campaign_recipients.valids.each do |recipient|
+      logger.debug "RECIPIENT: #{recipient.to_yaml}"
       if (email = recipient.recipient.email).present? && (emails & [email]).blank?
         contact = recipient.contact
+        logger.debug "CONTACT: #{contact.to_yaml}"
         if contact
           # EmailMailer.queue(:email, campaign, recipient, contact.name, contact.confirmation_code)
           EmailMailer.deliver_email!(campaign, recipient, contact.name, contact.confirmation_code)
